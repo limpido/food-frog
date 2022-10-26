2de21798dc2e579084aaf02960fa0507cbc2ab94
limpido <jiichuan0325@gmail.com>
2022-10-26 22:39:48 +0800

    home page

--- components/nav.php
+++ components/nav.php
@@ -1,12 +1,15 @@
 <?php
-  session_start();
+  if(!isset($_SESSION)) 
+  { 
+      session_start(); 
+  }
   if (isset($_SESSION['uid']) && isset($_SESSION['username'])) {
     echo '<nav>
     <a href="index.php">
     <img src="https://i.pinimg.com/736x/1a/e7/33/1ae733509d7ba91b55b034230249f77b.jpg">
     </a>
     <ul>
-        <li><a href="shoppingcart.html">Shopping Cart</a></li>
+        <li><a href="shopping-cart.php">Shopping Cart</a></li>
         <li><a href="orderhistory.html">Order History</a></li>
         <li><a href="profile.html">Profile</a></li>
         <li><a href="logout.php">Log out</a></li>
@@ -24,17 +27,4 @@
     </ul>
     </nav>';
   }
-
-//   echo '<nav>
-//     <a href="index.php">
-//     <img src="https://i.pinimg.com/736x/1a/e7/33/1ae733509d7ba91b55b034230249f77b.jpg">
-//     </a>
-//     <ul>
-//         <li><a href="shoppingcart.html">Shopping Cart</a></li>
-//         <li><a href="orderhistory.html">Order History</a></li>
-//         <li><a href="profile.html">Profile</a></li>
-//         <li><a href="login.php">Login</a></li>
-//         <li><a href="signup.php">Sign Up</a></li>
-//     </ul>
-//     </nav>'
 ?>
--- index.php
+++ index.php
@@ -3,13 +3,78 @@
 <head>
   <title>Food Frog</title>
   <link rel="stylesheet" href="style.css">
+  <style>
+    .container {
+      display: grid;
+      grid-template-columns: repeat(3, 30%);
+      column-gap: 32px;
+      row-gap: 32px;
+    }
+
+    .link:link, .link:visited {
+      color: #EFFCEF;
+      text-decoration: none;
+    }
+
+    .food-store {
+      display: flex;
+      flex-direction: column;
+      border-radius: 10px;
+      background-color: #655C56;
+      overflow: hidden;
+    }
+
+    .img-container{
+      height: 200px;
+      width: 100%;
+      overflow: hidden;
+    }
+
+    .store-img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+
+    .food-store:hover .store-img {
+      transform: scale(1.5);
+    }
+
+    .text-container {
+      padding: 16px;
+      display: flex;
+      flex-direction: column;
+    }
+
+    .store-title {
+      text-align: center;
+    }
+  </style>
 </head>
     <body>
         <header>
           <?php include 'components/nav.php';?>
         </header>
         <main>
-            <p>xxxx</p>
+            <div class="container">
+            <?php
+            include "dbconnect.php";
+
+            $query = "SELECT * FROM food_stores;";
+            $result = $db->query($query);
+
+            for ($i=0; $i < $result->num_rows; $i++) {
+              $store = $result->fetch_object();
+              echo '<a class="link" href="food-store.php?id='.$store->id.'"><div class="food-store">
+              <div class="img-container"><img class="store-img" src="' . $store->image. '"></div>
+              <div class="text-container">
+              <div class="store-title">'.$store->name.'</div>
+              <div class="cuisine-type"><span>'.$store->cuisine_type.'</span></div>
+              </div>
+            </div></a>';
+            }
+            ?>
+            </div>
         </main>
     </body>
   <?php include 'components/footer.php';?>
